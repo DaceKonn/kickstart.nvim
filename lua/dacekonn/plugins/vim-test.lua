275f94a5ffedb31d9a0d46fd9f82b664e5521adc
DaceKonn <dacekonn@gmail.com>
2024-12-07 09:57:23 +0100

    update keymap descriptions for vim-test

--- lua/dacekonn/plugins/vim-test.lua
+++ lua/dacekonn/plugins/vim-test.lua
@@ -1,11 +1,11 @@
 return {
   'vim-test/vim-test',
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>Tn', ':TestNearest<CR>', { noremap = true, silent = true, desc = 'Test [N]earest' })
-    vim.api.nvim_set_keymap('n', '<leader>Tc', ':TestClass<CR>', { noremap = true, silent = true, desc = 'Test [C]ass' })
-    vim.api.nvim_set_keymap('n', '<leader>Tf', ':TestFile<CR>', { noremap = true, silent = true, desc = 'Test [F]tile' })
-    vim.api.nvim_set_keymap('n', '<leader>Tl', ':TestLast<CR>', { noremap = true, silent = true, desc = 'Test [L]ast' })
-    vim.api.nvim_set_keymap('n', '<leader>Ts', ':TestSuite<CR>', { noremap = true, silent = true, desc = 'Test [S]uite' })
-    vim.api.nvim_set_keymap('n', '<leader>Tv', ':TestVisit<CR>', { noremap = true, silent = true, desc = 'Test [V]isit' })
+    vim.api.nvim_set_keymap('n', '<leader>Tn', ':TestNearest<CR>', { noremap = true, silent = true, desc = 'test [n]earest' })
+    vim.api.nvim_set_keymap('n', '<leader>Tc', ':TestClass<CR>', { noremap = true, silent = true, desc = 'test [C]ass' })
+    vim.api.nvim_set_keymap('n', '<leader>Tf', ':TestFile<CR>', { noremap = true, silent = true, desc = 'test [f]ile' })
+    vim.api.nvim_set_keymap('n', '<leader>Tl', ':TestLast<CR>', { noremap = true, silent = true, desc = 'show [l]ast result' })
+    vim.api.nvim_set_keymap('n', '<leader>Ts', ':TestSuite<CR>', { noremap = true, silent = true, desc = 'test [s]uite' })
+    vim.api.nvim_set_keymap('n', '<leader>Tv', ':TestVisit<CR>', { noremap = true, silent = true, desc = '[v]isit last run' })
   end,
 }
