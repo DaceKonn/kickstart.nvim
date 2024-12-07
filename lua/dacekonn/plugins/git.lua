f0e6db5faa509bcd44141e3985438de74d0f215f
DaceKonn <dacekonn@gmail.com>
2024-12-07 09:00:38 +0100

    Fixed warnings in Neogit configuration, enabled telescope integration

--- lua/dacekonn/plugins/git.lua
+++ lua/dacekonn/plugins/git.lua
@@ -154,17 +154,17 @@ return {
       integrations = {
         -- If enabled, use telescope for menu selection rather than vim.ui.select.
         -- Allows multi-select and some things that vim.ui.select doesn't.
-        telescope = nil,
+        telescope = true,
         -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
         -- The diffview integration enables the diff popup.
         --
         -- Requires you to have `sindrets/diffview.nvim` installed.
-        diffview = nil,
+        diffview = false,
 
         -- If enabled, uses fzf-lua for menu selection. If the telescope integration
         -- is also selected then telescope is used instead
         -- Requires you to have `ibhagwan/fzf-lua` installed.
-        fzf_lua = nil,
+        fzf_lua = false,
       },
       sections = {
         -- Reverting/Cherry Picking
