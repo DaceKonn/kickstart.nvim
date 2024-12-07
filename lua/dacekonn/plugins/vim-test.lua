46342fe359c69729e6d0570415ed1675d5154d85
DaceKonn <dacekonn@gmail.com>
2024-12-07 09:29:45 +0100

    playing with which key configs

--- lua/dacekonn/plugins/vim-test.lua
+++ lua/dacekonn/plugins/vim-test.lua
@@ -1,6 +1,14 @@
 return {
   'vim-test/vim-test',
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', { noremap = true, silent = true })
+    -- vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', { noremap = true, silent = true })
+    local wk = require 'which-key'
+    wk.add({
+      ['<leader>x'] = {
+        name = 'xxx',
+        x = { '<cmd>Telescope find_files<cr>', 'Find File' },
+        z = { name = 'zzz', z = { '<cmd>Telescope live_grep<cr>', 'Grep' }, y = { '<cmd>Telescope current_buffer_fuzzy_find<cr>', 'Buffer' } },
+      },
+    }, { prefix = '<leader>' })
   end,
 }
