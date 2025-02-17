8d1280812e833458a9efcbe63010125c4c85012d
Piotr Wolny <piotr.wolny@alan-systems.com>
2025-02-17 15:14:44 +0100

    Refactor - extract options from init.lua to separate files

---
+++ lua/config/neovide.lua
@@ -0,0 +1,5 @@
+-- NOTE: my neovide options
+-- [[ Neovide ]]
+vim.o.guifont = 'FiraCode Nerd Font'
+vim.opt.linespace = 15
+vim.g.neovide_scale_factor = 0.7
---
+++ lua/config/options.lua
@@ -0,0 +1,71 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- [Left column and similar]
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 20
+vim.opt.sidescrolloff = 8
+
+-- [Tab spacing/behavior]
+vim.opt.expandtab = true
+vim.opt.shiftwith = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- [General Behaviors]
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.backup = false
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+vim.opt.conceallevel = 0 -- show concealed characters in markdown files
+vim.opt.fileencoding = 'utf-8'
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.termguicolors = true
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+-- Save undo history
+vim.opt.undofile = true
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.writebackup = false
+vim.opt.cursorline = true -- Show which line your cursor is on
+
+-- [Searching behaviors]
+vim.opt.hlsearch = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
