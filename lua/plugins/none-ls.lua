d159f82d40e52109fb6e35e393695334075ef296
Piotr Wolny <piotr.wolny@alan-systems.com>
2025-02-18 17:40:58 +0100

    disable none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,21 @@
-return {
-  'nvimtools/none-ls.nvim',
-  dependencies = {
-    'nvimtools/none-ls-extras.nvim',
-  },
-  config = function()
-    -- get access to the none-ls functions -> yes, it's correct it should be null-ls
-    local null_ls = require 'null-ls'
-
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.stylua, --lua
-        require 'none-ls.diagnostics.eslint_d', -- javascript
-        null_ls.builtins.formatting.prettier, -- anything else
-      },
-    }
-
-    vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = '[C]ode [F]ormat' })
-  end,
-}
+return {}
+-- return {
+--   'nvimtools/none-ls.nvim',
+--   dependencies = {
+--     'nvimtools/none-ls-extras.nvim',
+--   },
+--   config = function()
+--     -- get access to the none-ls functions -> yes, it's correct it should be null-ls
+--     local null_ls = require 'null-ls'
+--
+--     null_ls.setup {
+--       sources = {
+--         null_ls.builtins.formatting.stylua, --lua
+--         require 'none-ls.diagnostics.eslint_d', -- javascript
+--         null_ls.builtins.formatting.prettier, -- anything else
+--       },
+--     }
+--
+--     vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = '[C]ode [F]ormat' })
+--   end,
+-- }
