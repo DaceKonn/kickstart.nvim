1d4cd80a5afac9dcc4fa5cd7ce582e40954a6ba2
Piotr Wolny <piotr.wolny@alan-systems.com>
2025-02-18 17:24:28 +0100

    Fix java home folder

--- lua/config/jdtls.lua
+++ lua/config/jdtls.lua
@@ -45,7 +45,7 @@ end
 
 local function get_workspace()
   -- Get the home directory of your operating system
-  local home = os.getenv 'HOME'
+  local home = os.getenv 'USERPROFILE' or os.getenv 'HOME'
   -- Declare a directory where you would like to store project information
   local workspace_path = home .. '/code/workspace/'
   -- Determine the project name
